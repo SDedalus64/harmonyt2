94c655cfd6fa9bc0b577ac896ef7bfe7a17586da
SDedalus64 <164300694+SDedalus64@users.noreply.github.com>
2025-06-18 12:40:17 -0700

    fix babel config for flow

--- babel.config.js
+++ babel.config.js
@@ -2,8 +2,7 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [
-      'babel-preset-expo',
-      '@babel/preset-typescript'
+      'babel-preset-expo'
     ],
     plugins: [
       'react-native-reanimated/plugin'
